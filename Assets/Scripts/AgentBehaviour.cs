8c7f87c574390ffdde458a5829452ee2c2fdb977
Steffan Mouton <smouton8993@gmail.com>
2019-05-13 18:11:54 -0500

    Initial commit

---
+++ Assets/Scripts/AgentBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour
+{
+	[SerializeField] private Transform targetTransform;
+	[SerializeField] private GameObject targetGameObject;
+	[SerializeField] private Vector3 velocity;
+
+	public Transform TargetTransform
+	{
+		get { return targetTransform; }
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+/// <summary>
+/// debug.drawlines in update
+/// </summary>
+	private void LateUpdate()
+	{
+		transform.position += velocity * Time.deltaTime;
+	}
+
+	public void AddForce(Vector3 force)
+	{
+		velocity += force;
+	}
+}
