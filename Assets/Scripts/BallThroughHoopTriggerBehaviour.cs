585b186ae7332a705b0cdfcd8967fcf146b51b44
Steffan Mouton <smouton8993@gmail.com>
2019-06-21 10:13:43 -0500

    Basketball Morning Exercise

---
+++ Assets/Scripts/BallThroughHoopTriggerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallThroughHoopTriggerBehaviour : MonoBehaviour
+{
+	public BasketballControllerBehaviour bbController;
+	public Text scoreView;
+	public GameObject scoresplosion;
+	
+	public void OnTriggerEnter(Collider other)
+	{
+		
+		if (!other.gameObject.CompareTag("Ball"))
+			return;
+
+		Instantiate(scoresplosion);
+		scoresplosion.transform.position = transform.position;
+		bbController.Score++;
+		scoreView.text = "Score: " + bbController.Score;
+
+	}
+}
---
+++ Assets/Scripts/BasketballControllerBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasketballControllerBehaviour : MonoBehaviour
+{
+
+	public GameObject bball;
+
+	public Vector3 shootForce;
+	public int Score;
+	public Text scoreView;
+	// Use this for initialization
+	void Start ()
+	{
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			ShootIt();
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			ShootIt();
+		}
+	}
+
+	public void ShootIt()
+	{
+		var ballInstance = Instantiate(bball);
+		var ballRB = ballInstance.GetComponent<Rigidbody>();
+		
+		ballInstance.transform.position = transform.position + Vector3.forward * 10;
+		ballRB.AddForce(shootForce);
+	}
+}
